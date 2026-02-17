cd98619c0b0b372026cc7f2788c670dfd3271af6
marteksydtveit <marte.sydtveit@gmail.com>
2026-02-17 15:23:22 +0100

    Me Forgot Branch

---
+++ Main.py
@@ -0,0 +1,15 @@
+
+#%% Load package
+
+import pandas as pd
+
+
+#%% Load data
+
+OccRoom1 = pd.read_csv("OccupancyRoom1.csv")
+OccRoom2 = pd.read_csv("OccupancyRoom2.csv")
+Price = pd.read_csv("PriceData.csv")
+
+
+
+#%%
